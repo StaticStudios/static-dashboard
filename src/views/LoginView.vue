d6dc4e74d6bdadb682d9d278ae0602f6dc3a2460
leguan <longboard.noah@gmail.com>
2025-09-21 12:45:38 +0200

    update LoginView with card layout and branding enhancements

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,6 +3,7 @@
 import {Button} from "@/components/ui/button";
 import {useUserStore} from "@/stores/UserStore.ts";
 import {useRouter} from "vue-router";
+import {Card, CardContent, CardHeader} from "@/components/ui/card";
 
 const userStore = useUserStore()
 const router = useRouter()
@@ -14,9 +15,31 @@ function login() {
 </script>
 
 <template>
-  <Button v-on:click="login">
-    Login
-  </Button>
+<!--  <Button v-on:click="login">-->
+<!--    Login-->
+<!--  </Button>-->
+  <div class="flex justify-center  p-75">
+    <Card class="w-96">
+      <CardHeader>
+        <div class="flex items-center justify-center">
+          <img src="/logo.png" alt="logo" class="h-28 w-28"></img>
+        </div>
+        <div class="text-5xl font-bold">
+          Static Studios
+        </div>
+        <div class="text-xl font-semibold">
+          Admin Dashboard
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div class="flex flex-col gap-4">
+          <Button v-on:click="login">
+            Login
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
 </template>
 
 <style scoped>
